4f462b68687af9fee394251bb8f00964cc903f83
NewBean0312 <newbean0312@gmail.com>
2023-12-20 18:15:22 +0900

    PokemonList 작업 2

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,17 @@
-export default function PokemonList({ key, imgSrc, pokemonName, id }) {
+export default function PokemonList({ key, imgSrc,pokemonName, id, pokemonSpecies }) {
   return (
     <>
-      <li className="w-52 h-60 mt-6 mb-2 bg-white">
-        <div className="grid flex-col" key={key} >
-          <img className="w-3/4 justify-self-center" src={imgSrc} alt={pokemonName} />
-          <p className="text-[color:var(--mui-color-text-main)]">{pokemonName}</p>
-          <p className="text-[color:var(--mui-color-text-main)]">ID : {id}</p>
+      <li className="w-52 h-60 mt-6 mb-2 border-gray-900 border-2">
+        <div className="grid flex-col" key={key}>
+          <img
+            className="w-4/5 justify-self-center"
+            src={imgSrc}
+            alt={pokemonName}
+          />
+          <p className="ml-2 text-gray-500">No.{id}</p>
+          <p className="mt-2 ml-2 text-xl text-[color:var(--mui-color-text-main)]">
+            {pokemonName}
+          </p>
         </div>
       </li>
     </>
--- src/pages/PokedexPage.js
+++ src/pages/PokedexPage.js
@@ -1,16 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import PokemonList from "../components/PokemonList";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Pokedex = () => {
   const [pokemonData, setPokemonData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pokemonPerPage = 76;
+  const totalPokemon = 152;
 
   // 데이터 받아오기
   useEffect(() => {
     // api 가져오기
     const fetchData = async () => {
       const allPokemonData = [];
-      for (let i = 1; i < 152; i++) {
+      for (
+        let i =1;
+        i < Math.min(currentPage * pokemonPerPage, totalPokemon);
+        i++
+      ) {
         const response = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${i}`
         );
@@ -26,23 +34,33 @@ const Pokedex = () => {
     };
 
     fetchData();
-  }, []);
+  }, [currentPage]);
+
+  const fetchMoreData = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
   return (
-    <>
-      <div className="flex justify-center w-screen bg-white">
-        <ul className="flex flex-wrap justify-around w-4/5 bg-red-500 mt-10">
-          {pokemonData.map((pokemon) => (
-            <PokemonList
-              key={pokemon.id}
-              imgSrc={pokemon.sprites.front_default}
-              pokemonName={pokemon.kor_name}
-              id={pokemon.id}
-            />
-          ))}
-        </ul>
-      </div>
-    </>
+      <InfiniteScroll
+        dataLength={pokemonData.length}
+        next={fetchMoreData}
+        hasMore={currentPage * pokemonPerPage}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p>All Pokémon have been loaded</p>}
+      >
+        <div className="flex justify-center w-screen bg-white">
+          <ul className="flex flex-wrap justify-around mt-10 w-4/5 ">
+            {pokemonData.map((pokemon) => (
+              <PokemonList
+                key={pokemon.id}
+                imgSrc={pokemon.sprites.front_default}
+                id={pokemon.id}
+                pokemonName={pokemon.kor_name}
+              />
+            ))}
+          </ul>
+        </div>
+      </InfiniteScroll>
   );
 };
 
