bc3b09e7ffd77b8fb1b3ce7d18ac8ef521d0ba6b
NewBean0312 <newbean0312@gmail.com>
2023-12-28 12:02:54 +0900

    상세 페이지 UI 작업

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -5,6 +5,7 @@ const PokemonDetail = ({ pokemon }) => {
     return <p>Loading...</p>;
   }
 
+  // 속성
   const renderTypes = () => {
     return pokemon.types.map((type, index) => (
       <span key={type.type.name}>
@@ -14,7 +15,7 @@ const PokemonDetail = ({ pokemon }) => {
     ));
   };
 
-
+  // 특성
   const renderAbilities = () => {
     return pokemon.abilities.map((ability, index) => (
       <span key={ability.ability.name}>
@@ -24,9 +25,10 @@ const PokemonDetail = ({ pokemon }) => {
     ));
   };
 
+  // 기술
   const renderMoves = () => {
-    return pokemon.moves.map((move) => (
-      <li key={move.move.name}>{move.move.korean_name}</li>
+    return pokemon.moves.map((move, index) => (
+      <li className={index % 2 == 0 ? "bg-yellow-200" : "bg-yellow-100"} key={move.move.name}>{move.move.korean_name}</li>
     ));
   };
 
@@ -40,15 +42,15 @@ const PokemonDetail = ({ pokemon }) => {
             alt={pokemon.korean_name}
           />
         </div>
-        <div className="w-2/3 pt-10 bg-red-300">
-          <p> #{pokemon.id}</p>
-          <h2 className="text-5xl text-black">{pokemon.korean_name}</h2>
-          <p>속성: {renderTypes()}</p>
-          <p>특성: {renderAbilities()}</p>
-          <p>키: {pokemon.height / 10}m</p>
-          <p>무게: {pokemon.weight / 10}kg</p>
-          <p>기술:</p>
-          <ul>{renderMoves()}</ul>
+        <div className="w-2/3 pt-10">
+          <p className="text-2xl text-gray-600"> #{pokemon.id}</p>
+          <h2 className="pt-2 text-5xl text-black">{pokemon.korean_name}</h2>
+          <p className="pt-2 text-xl">속성: {renderTypes()}</p>
+          <p className="pt-2 text-xl">특성: {renderAbilities()}</p>
+          <p className="pt-2 text-xl">키: {pokemon.height / 10}m</p>
+          <p className="pt-2 text-xl">무게: {pokemon.weight / 10}kg</p>
+          <p className="pt-2 text-xl">기술:</p>
+          <ul className="overflow-y-scroll w-64 h-64 text-lg">{renderMoves()}</ul>
         </div>
       </div>
     </div>
