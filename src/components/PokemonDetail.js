db77c27352b27dc8d5d8114a92aa252d14f9204c
NewBean0312 <newbean0312@gmail.com>
2023-12-28 12:42:06 +0900

    상세 페이지 포켓몬 모든 속성 색상 적용

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -11,10 +11,42 @@ const PokemonDetail = ({ pokemon }) => {
       <span
         key={type.type.name}
         className={
-          type.type.korean_name === "풀"
+          type.type.korean_name === "노말"
+            ? "text-gray-400"
+            : type.type.korean_name === "불꽃"
+            ? "text-red-600"
+            : type.type.korean_name === "물"
+            ? "text-blue-600"
+            : type.type.korean_name === "풀"
             ? "text-green-600"
+            : type.type.korean_name === "전기"
+            ? "text-yellow-600"
+            : type.type.korean_name === "얼음"
+            ? "text-blue-400"
+            : type.type.korean_name === "격투"
+            ? "text-red-800"
             : type.type.korean_name === "독"
+            ? "text-purple-800"
+            : type.type.korean_name === "땅"
+            ? "text-yellow-900"
+            : type.type.korean_name === "비행"
+            ? "text-indigo-400"
+            : type.type.korean_name === "에스퍼"
+            ? "text-pink-600"
+            : type.type.korean_name === "벌레"
+            ? "text-green-900"
+            : type.type.korean_name === "바위"
+            ? "text-gray-800"
+            : type.type.korean_name === "고스트"
             ? "text-purple-600"
+            : type.type.korean_name === "드래곤"
+            ? "text-blue-800"
+            : type.type.korean_name === "악"
+            ? "text-gray-800"
+            : type.type.korean_name === "강철"
+            ? "text-indigo-200"
+            : type.type.korean_name === "페어리"
+            ? "text-pink-300"
             : ""
         }
       >
@@ -40,7 +72,7 @@ const PokemonDetail = ({ pokemon }) => {
   const renderMoves = () => {
     return pokemon.moves.map((move, index) => (
       <li
-        className={index % 2 == 0 ? "bg-yellow-200" : "bg-yellow-100"}
+        className={index % 2 == 0 ? "bg-blue-200" : "bg-blue-100"}
         key={move.move.name}
       >
         {move.move.korean_name}
