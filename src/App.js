e692fd7ace2a1f9221b2b568ebf72425ba01ef40
NewBean0312 <newbean0312@gmail.com>
2024-01-09 15:45:03 +0900

    타이틀 변경 및 메인 홈 링크 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { AppBar, Toolbar } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import PokedexPage from "./pages/PokedexPage";
 import PokemonDetailPage from "./pages/PokemonDetailPage";
 // import Search from "./components/Search";
@@ -9,7 +9,7 @@ function App() {
     <>
       <AppBar position="static">
         <Toolbar className="flex font-bold text-white text-xl">
-          <h2>PokemonDex</h2>
+          <a href="/">PokemonDex</a>
           {/* <span className="flex-grow"></span><Search /> */}
         </Toolbar>
         <Router>
